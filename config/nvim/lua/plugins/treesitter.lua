c1b11583d40ba0befe1fa0666fde9cce004be081
Herman Berdnikov <berdnikov1802@gmail.com>
2026-06-09 22:57:22 -0400

    Update

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -27,35 +27,21 @@ return {
 			"vim",
 			"dockerfile",
 			"gitignore",
+			"gitcommit",
 		}
 
 		ts.install(ensure_installed)
 
 		vim.api.nvim_create_autocmd("FileType", {
-			group = vim.api.nvim_create_augroup("treesitter_auto_start", { clear = true }),
 			callback = function(args)
 				local buf = args.buf
 				local ft = vim.bo[buf].filetype
 
-				if ft == "" or ft == "NvimTree" then
+				if ft == "" then
 					return
 				end
 
-				-- Try to start treesitter.
-				local ok = pcall(vim.treesitter.start, buf)
-
-				if ok then
-					return
-				end
-
-				-- If parser is missing, try installing parser matching the filetype,
-				-- then start treesitter after install finishes.
-				local install_ok, install_result = pcall(ts.install, { ft })
-
-				if install_ok and install_result then
-					install_result:wait(300000)
-					pcall(vim.treesitter.start, buf)
-				end
+				pcall(vim.treesitter.start, buf)
 			end,
 		})
 	end,
